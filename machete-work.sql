310bc346a8a9986d948226571ee65d32435fc723
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-30 14:23:21 -0700

    Changing work schema.

--- machete-work.sql
+++ machete-work.sql
@@ -5,6 +5,7 @@ CREATE SCHEMA IF NOT EXISTS work;
 
 CREATE TABLE IF NOT EXISTS work.order(
     "id" uuid PRIMARY KEY DEFAULT gen_random_uuid()
+  , "employerProfileId" integer NOT NULL REFERENCES employer.profile
   , "englishMasteryRequired" smallint NOT NULL
   , "workerSkillId" integer NOT NULL REFERENCES worker.skill
   , "masteryRequired" smallint NOT NULL
@@ -13,25 +14,23 @@ CREATE TABLE IF NOT EXISTS work.order(
   , "active" boolean NOT NULL
 );
 
+-- option: use postGIS for location f(x)s
 CREATE TABLE IF NOT EXISTS work.offer (
     "id" SERIAL PRIMARY KEY
   , "orderId" uuid REFERENCES work.order
   , "workerProfileId" integer REFERENCES worker.profile
   , "meetsSponsorshipRequirements" boolean NOT NULL
+  , "location" point NOT NULL
+  , "transportationMethodId" integer NOT NULL
   , "timePromised" timestamp NOT NULL
   , "counterOffer" decimal NOT NULL
   , "active" boolean NOT NULL
 );
 
+-- seriously wondering if active should be even allowed here--this is final acceptance
 CREATE TABLE IF NOT EXISTS work.assignment(
     "id" SERIAL PRIMARY KEY
-  , "orderId" uuid REFERENCES work.order
-  , "workerProfileId" integer REFERENCES worker.profile
-  , "distanceFromWorker" decimal NOT NULL
-  , "transportationMethodId" integer NOT NULL
+  , "offerId" integer REFERENCES work.offer
   , "acceptedWage" decimal NOT NULL
   , "active" boolean NOT NULL
 );
-
--- demographic information is contained in the report schema
-

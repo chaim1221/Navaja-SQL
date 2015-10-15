266376fcbe53fbf4bf6bd27c7477cc2beebd04aa
Chaim Eliyah <chaimeliyah@gmail.com>
2015-10-15 14:07:33 -0700

    This works, without the output I want...

--- app.js
+++ app.js
@@ -1,81 +1,59 @@
 var exec = require("child_process").exec;
 var fs = require("fs");
-var err = function (error, stdout, stderr) { 
-    console.log(error || "");
-    console.log(stdout || "");
-    console.log(stderr || "");
+
+var child = function (cmd, msg, callback) {
+    exec(cmd, function (result) {
+        finished(result);
+        callback(msg);
+    });
 }
 
-var returnedResults = main();
-for (var i = 0; i < returnedResults.length; i++) {
-    console.log(returnedResults[i]);
-}
-process.exit();
-
-function main () {
-    var results = [];
-    results.push(createUser(err));
-    results.push(createDb(err));
-    results.push(createEmployer(err));
-    results.push(createWorker(err));
-    results.push(createWork(err));
-    return results;
+// TODO find a way to use this
+var finished = function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) console.log('exec error: ' + error);
 }
 
-function createUser (err) { 
-    try {
-        console.log("Creating DB user...");
-        exec("psql -U postgres -d postgres -a -f machete-db-user.sql", err);
-        console.log("Appending DB user password to .pgpass file...");
-        fs.appendFile(process.env.HOME + "/.pgpass", "*:*:machetedb:machetedb_app_user:replace_me\n", err);
-        return "Successfully created DB user.";
-    }
-    catch (error) {
-        return "Error creating DB user.";
-    }
+function createUser (callback) { 
+    child("psql -U postgres -d postgres -a -f machete-db-user.sql", "Created DB user.", callback);
 }
 
-function createDb (err) {
-    try {
-        console.log("Creating DB...");
-        exec("export PGPASSWORD='replace_me'; psql -U machetedb_app_user -d postgres -a -f machete-db.sql; export PGPASSWORD=''", err);
-        return "Successfully created DB.";
-    }
-    catch (error) {
-        return "Error creating DB.";
-    }
+function savePassword (callback) {
+    fs.appendFile(process.env.HOME + "/.pgpass", "*:*:machetedb:machetedb_app_user:replace_me\n", callback("Appended password to .pgpass file."));
 }
 
-function createEmployer (err) {
-    try {
-        console.log("Creating employer schema...");
-        exec("psql -U machetedb_app_user -d machetedb -a -f machete-employer.sql", err);
-        return "Successfully created employer schema.";
-    }
-    catch (error) {
-        return "Error creating employer schema.";
-    }
+function createDb (callback) {
+    child("export PGPASSWORD='replace_me'; psql -U machetedb_app_user -d postgres -a -f machete-db.sql; export PGPASSWORD=''", "Created DB.", callback);
 }
 
-function createWorker (err) {
-    try {
-        console.log("Creating worker schema...");
-        exec("psql -U machetedb_app_user -d machetedb -a -f machete-worker.sql", err);
-        return "Successfully created worker schema.";
-    }
-    catch (error) {
-        return "Error creating worker schema.";
-    }
+function createEmployer (callback) {
+    child("psql -U machetedb_app_user -d machetedb -a -f machete-employer.sql", "Created employer schema.", callback);
 }
 
-function createWork (err) {
-    try {
-        console.log("Creating work schema...");
-        exec("psql -U machetedb_app_user -d machetedb -a -f machete-work.sql", err);
-        return "Successfully created work schema.";
-    }
-    catch (error) {
-        return "Error creating work schema.";
-    }
+function createWorker (callback) {
+    child("psql -U machetedb_app_user -d machetedb -a -f machete-worker.sql", "Created worker schema.", callback);
 }
 
+function createWork (callback) {
+    child("psql -U machetedb_app_user -d machetedb -a -f machete-work.sql", "Created work schema.", callback);
+}
+
+createUser(function (callback) { 
+    console.log(callback); 
+    savePassword(function (callback) {
+        console.log(callback);
+        createDb(function (callback) {
+            console.log(callback);
+            createEmployer(function (callback) {
+                console.log(callback);
+                createWorker(function (callback) {
+                    console.log(callback);
+                    createWork(function (callback) {
+                        console.log(callback);
+                    });
+                });
+            });
+        });
+    });
+});
